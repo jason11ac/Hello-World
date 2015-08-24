cabfe9103c4d3b25f63e45dc3c9a7cc57740da06
jason11ac <jason11ac@gmail.com>
2015-08-24 10:28:56 -0700

    Header file for global constants

---
+++ globals.h
@@ -0,0 +1,17 @@
+  
+#ifndef CS_32_Project_3_globals_h
+#define CS_32_Project_3_globals_h
+
+//Grid global constants
+
+#include <iostream>
+#include <string>
+
+const int MAXROWS = 28;     //Max rows for dungeon
+const int MAXCOLS = 100;     //Max cols for dungeon
+
+// 18, 70
+
+// 30, 90
+
+#endif
